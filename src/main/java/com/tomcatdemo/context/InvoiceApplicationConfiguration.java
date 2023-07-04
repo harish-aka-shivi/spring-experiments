5714d447b1d19064088013daf105760d1c17af75
Harish <r.shivi@yahoo.com>
2023-07-04 17:48:43 +0530

    feat: add jdbc

--- src/main/java/com/tomcatdemo/context/InvoiceApplicationConfiguration.java
+++ src/main/java/com/tomcatdemo/context/InvoiceApplicationConfiguration.java
@@ -2,15 +2,22 @@ package com.tomcatdemo.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tomcatdemo.ApplicationLauncher;
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -18,11 +25,34 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
         , ignoreResourceNotFound = true)
 @EnableWebMvc
+@EnableTransactionManagement
 public class InvoiceApplicationConfiguration {
     @Bean //
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
+
+
+    @Bean
+    public DataSource dataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:~/myFirstH2Database;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public TransactionManager platformTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
--- src/main/java/com/tomcatdemo/service/InvoiceService.java
+++ src/main/java/com/tomcatdemo/service/InvoiceService.java
@@ -6,22 +6,31 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
 
-    List<Invoice> invoices = new CopyOnWriteArrayList<>(); //
-
     private final UserService userService;
     private final String cdnUrl;
 
-    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
+    private final JdbcTemplate jdbcTemplate;
+
+    public InvoiceService(UserService userService, JdbcTemplate jdbcTemplate, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
         this.cdnUrl = cdnUrl;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
 
@@ -37,22 +46,60 @@ public class InvoiceService {
         // TODO actual deletion of PDFs
     }
 
+    @Transactional
     public List<Invoice> findAll() {
-        return invoices;
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+
+//        return invoices;
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Invoice invoice = new Invoice();
+            invoice.setId(resultSet.getObject("id").toString());
+            invoice.setPdfUrl(resultSet.getString("pdf_url"));
+            invoice.setUserId(resultSet.getString("user_id"));
+            invoice.setAmount(resultSet.getInt("amount"));
+            return invoice;
+        });
     }
 
 
+    @Transactional
     public Invoice create(String userId, Integer amount) {
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
 
-        User user =  userService.findById(userId);
-        if (user == null) {
-            throw new IllegalStateException();
-        }
+        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
 
-        // TODO real pdf creation and storing it on network server
-        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
-        invoices.add(invoice);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, userId);  //
+            ps.setString(2, generatedPdfUrl);
+            ps.setInt(3, amount);
+            return ps;
+        }, keyHolder);
+
+        String uuid = !keyHolder.getKeys().isEmpty()
+                ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString()
+                : null;
+
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPdfUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
         return invoice;
+
+        //        User user =  userService.findById(userId);
+//        if (user == null) {
+//            throw new IllegalStateException();
+//        }
+//
+//        // TODO real pdf creation and storing it on network server
+//        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
+//        invoices.add(invoice);
+//        return invoice;
     }
 
 }
